dee0f47802d2e30f392c21fb5b4e55a5fe27d167
94Pisces <18702629956@163.com>
2019-09-06 16:55:53 +0800

    Update Info.php

--- application/api/controller/Info.php
+++ application/api/controller/Info.php
@@ -384,7 +384,7 @@ class Info extends Common
                 }
 
             }
-            dump($data);exit;
+            dump($input_data);exit;
             $validate =new \app\common\validate\Video();
             $validate->scene('api_release');
             $model = new \app\common\model\Video();
