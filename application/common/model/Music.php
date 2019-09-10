917f3a23f332a5f4ecd59821e4bdcddda79b53a7
94Pisces <18702629956@163.com>
2019-09-10 15:12:38 +0800

    add

--- application/common/model/Music.php
+++ application/common/model/Music.php
@@ -17,9 +17,31 @@ class Music extends BaseModel
     }
 
 
-    protected function setDurationAttr($value)
+//    protected function setDurationAttr($value)
+//    {
+//        return empty($value)?'00:00:00':sprintf('%2d:%2d:%2d',intval($value/60/60),intval($value/60),intval($value%60));
+//    }
+
+
+    public static function init()
     {
-        return empty($value)?'00:00:00':sprintf('%2d:%2d:%2d',intval($value/60/60),intval($value/60),intval($value%60));
+        parent::init();
+
+        self::event('after_update',function($model){
+            $model->handleMusicFile();
+        });
+
+        self::event('after_insert',function($model){
+            $model->handleMusicFile();
+        });
+
     }
 
+    //保存音频到本地
+    public function handleMusicFile()
+    {
+        save_music($this->file);
+
+
+    }
 }
