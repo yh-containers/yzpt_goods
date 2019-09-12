8f360ef1241b14ac96f73fab203411efda5bee2b
94Pisces <18702629956@163.com>
2019-09-12 18:47:00 +0800

    Update Users.php

--- application/common/model/Users.php
+++ application/common/model/Users.php
@@ -307,6 +307,7 @@ class Users extends BaseModel
             if($auth_info['mode']=='wechat'){
                 $auth_user_info = \app\common\service\third\OpenWx::actToUserInfo($auth_info['access_token'],$auth_info['open_id']);
                 $data['wx_openid'] = (empty($auth_user_info)||empty($auth_user_info['unionid']))?$auth_info['open_id']:$auth_user_info['unionid'];
+                $data['thrd_info']=$auth_user_info;
                 $third_update['wx_openid']='';
             }elseif ($auth_info['mode']=='qq'){
                 $data['qq_openid'] = $auth_info['open_id'];
