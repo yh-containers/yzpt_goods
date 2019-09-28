16b2d6bc00c0c651a1d46d3a4aedf6829790b0b9
yh <1104753531@qq.com>
2019-09-28 11:39:20 +0800

    add

--- application/common/model/Users.php
+++ application/common/model/Users.php
@@ -256,7 +256,7 @@ class Users extends BaseModel
             $award_times = UsersRaiseLogs::where([['uid','=',$model['id']],['type','=',8],['create_time','>=',date('Y-m-d').' 00:00:00']])->count();
             if($award_num>0 && (empty($award_limit) || empty($award_times) || $award_times<$award_limit)){
                 //验证用户是否有更新记录
-                $update_record_model = UsersHealth::where(['date'=>date('Y-m-d'),'uid'=>$model['uid']])->find();
+                $update_record_model = UsersHealth::where(['date'=>date('Y-m-d'),'uid'=>$model['id']])->find();
                 if($update_record_model){
                     //增加养分
                     $model->recordRaise($award_num, 8,'更新健康信息获得:'.$award_num.'养分');
