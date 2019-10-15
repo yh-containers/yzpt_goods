856421628fc8f03c798c70193588e582c526924d
94Pisces <53338283+94Pisces@users.noreply.github.com>
2019-10-15 14:09:47 +0800

    Update Cart.php

--- application/common/model/Cart.php
+++ application/common/model/Cart.php
@@ -32,6 +32,7 @@ class Cart extends BaseModel
             $info['num'] = $res['num']+$data['num'];//array(self::raw('num+'.$data['num']));
             return $info;
         }else{
+            $data['id'] = '';
             return $data;
         }
     }
